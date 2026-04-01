11d5e7f017f4f3943576f773300556f0c63fbeca
KryptoBudowlaniec <129612869+KryptoBudowlaniec@users.noreply.github.com>
2026-04-01 17:38:40 +0200

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -436,7 +436,7 @@ elif branza == "📐 Podłogi (Panele/Deska)":
     
     with col_img:
         # Możesz użyć zdjęcia z Unsplash (darmowe) lub wgrać własne na GitHub
-        st.image("jodelka.jpg", use_column_width=True)
+        st.image("https://files.fm/u/qazpkf5w64", use_column_width=True)
         
         # Opcjonalnie: Dodaj małą ramkę pod zdjęciem z info
         st.info("Pamiętaj o dodaniu 10% zapasu na docięcia!")
